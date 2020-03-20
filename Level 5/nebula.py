dfbacafe6e3e4afdeae0917fbe63aad655d80b79
pedrodemaia <pedrodemaia@gmail.com>
2020-03-20 16:17:21 -0300

    Inclusion of Level 5 challenge

--- Level 5/nebula.py
+++ Level 5/nebula.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 17 08:33:00 2020
+Created on Fri Mar 20 09:54:09 2020
 
 @author: pedro
 """
+
 
 # Case 1
 g = [[True, False, True], 
@@ -26,209 +27,48 @@ g = [[True, True, False, True, False, True, False, True, True, False],
      [False, True, False, False, False, False, True, True, False, False]]
 output = 11567
 
-def copySolution(p):
-    return [list(p[i]) for i in range(len(p))]
+from collections import defaultdict
+
+# get next step for row r1 considering the lower row r2
+def getResult(r1, r2, nCols):
+    c11 = r1 >> 1 # upper row [:-1] 
+    c12 = r1 & ~(1 << nCols) # upper row [1:]
+    c21 = r2 >> 1  # lower row [:-1]
+    c22 = r2 & ~(1 << nCols) # lower row [1:]
+    
+    return (c11 & ~c12 & ~c21 & ~c22) | (~c11 & c12 & ~c21 & ~c22) | \
+        (~c11 & ~c12 & c21 & ~c22) | (~c11 & ~c12 & ~c21 & c22)
 
 def solution(g):
-    # Your code here
-    def backPropagate(g, p, r, c):
-        nextR, nextC = (r, c-1) if c > 0 else (r-1, len(g[r])-1)
-            
-        if r == len(g)-1:
-            # last row
-            s = p[r][c+1] + p[r+1][c+1]
-            if g[r][c]:
-                if s == 0:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r+1][c] = 1, 0
-                    p2 = copySolution(p)
-                    p2[r][c], p2[r+1][c] = 0, 1
-                    return backPropagate(g, p1, nextR, nextC) + \
-                        backPropagate(g, p2, nextR, nextC)
-                elif s == 1:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r+1][c] = 0, 0
-                    return backPropagate(g, p1, nextR, nextC)
-                else:
-                    return 0
-            else:
-                if s == 0:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r+1][c] = 0, 0
-                    p2 = copySolution(p)
-                    p2[r][c], p2[r+1][c] = 1, 1
-                    return backPropagate(g, p1, nextR, nextC) + \
-                        backPropagate(g, p2, nextR, nextC)
-                elif s == 1:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r+1][c] = 0, 1
-                    p2 = copySolution(p)
-                    p2[r][c], p2[r+1][c] = 1, 0
-                    p3 = copySolution(p)
-                    p3[r][c], p3[r+1][c] = 1, 1
-                    return backPropagate(g, p1, nextR, nextC) + \
-                        backPropagate(g, p2, nextR, nextC) + \
-                        backPropagate(g, p3, nextR, nextC)
-                else:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r+1][c] = 0, 0
-                    p2 = copySolution(p)
-                    p2[r][c], p2[r+1][c] = 0, 1
-                    p3 = copySolution(p)
-                    p3[r][c], p3[r+1][c] = 1, 0
-                    p4 = copySolution(p)
-                    p4[r][c], p4[r+1][c] = 1, 1
-                    return backPropagate(g, p1, nextR, nextC) + \
-                        backPropagate(g, p2, nextR, nextC) + \
-                        backPropagate(g, p3, nextR, nextC) + \
-                        backPropagate(g, p4, nextR, nextC)
-                        
-        elif c == len(g[r])-1:
-            # las col
-            s = p[r+1][c] + p[r+1][c+1]
-            if g[r][c]:
-                if s == 0:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r][c+1] = 0, 1
-                    p2 = copySolution(p)
-                    p2[r][c], p2[r][c+1] = 1, 0
-                    return backPropagate(g, p1, nextR, nextC) + \
-                        backPropagate(g, p2, nextR, nextC)
-                elif s == 1:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r][c+1] = 0, 0
-                    return backPropagate(g, p1, nextR, nextC)
-                else:
-                    return 0
-            else:
-                if s == 0:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r][c+1] = 0, 0
-                    p2 = copySolution(p)
-                    p2[r][c], p2[r][c+1] = 1, 1
-                    return backPropagate(g, p1, nextR, nextC) + \
-                        backPropagate(g, p2, nextR, nextC)
-                elif s == 1:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r][c+1] = 0, 1
-                    p2 = copySolution(p)
-                    p2[r][c], p2[r][c+1] = 1, 0
-                    p3 = copySolution(p)
-                    p3[r][c], p3[r][c+1] = 1, 1
-                    return backPropagate(g, p1, nextR, nextC) + \
-                        backPropagate(g, p2, nextR, nextC) + \
-                        backPropagate(g, p3, nextR, nextC)
-                else:
-                    p1 = copySolution(p)
-                    p1[r][c], p1[r][c+1] = 0, 0
-                    p2 = copySolution(p)
-                    p2[r][c], p2[r][c+1] = 0, 1
-                    p3 = copySolution(p)
-                    p3[r][c], p3[r][c+1] = 1, 0
-                    p4 = copySolution(p)
-                    p4[r][c], p4[r][c+1] = 1, 1
-                    return backPropagate(g, p1, nextR, nextC) + \
-                        backPropagate(g, p2, nextR, nextC) + \
-                        backPropagate(g, p3, nextR, nextC) + \
-                        backPropagate(g, p4, nextR, nextC)
-        
-        elif r == 0 and c == 0:
-            # 1st cell
-            s = p[0][1] + p[1][0] + p[1][1]
-            if g[0][0]:
-                if s <= 1:
-                    return 1
-                else:
-                    return 0
-            else:
-                if s <= 1:
-                    return 1
-                else:
-                    return 2
-            
-        else:
-            # common case
-            s = p[r][c+1] + p[r+1][c] + p[r+1][c+1]
-            if g[r][c]:
-                if s == 0:
-                    p1 = copySolution(p)
-                    p1[r][c] = 1
-                    return backPropagate(g, p1, nextR, nextC)
-                elif s == 1:
-                    p1 = copySolution(p)
-                    p1[r][c] = 0
-                    return backPropagate(g, p1, nextR, nextC)
-                else:
-                    return 0
-            else:
-                if s == 0:
-                    p1 = copySolution(p)
-                    p1[r][c] = 0
-                    return backPropagate(g, p1, nextR, nextC)
-                elif s == 1:
-                    p1 = copySolution(p)
-                    p1[r][c] = 1
-                    return backPropagate(g, p1, nextR, nextC)
-                else:
-                    p1 = copySolution(p)
-                    p1[r][c] = 0
-                    p2 = copySolution(p)
-                    p2[r][c] = 1
-                    return backPropagate(g, p1, nextR, nextC) + \
-                        backPropagate(g, p2, nextR, nextC)
+    rows, cols = len(g), len(g[0])
+    # transpose g to improve performance operating on lowest dimension
+    if cols > rows:
+        g = list(zip(*g))
+        rows, cols = cols, rows
     
-    m, n = len(g), len(g[0])
-
-    p = [[-1 for i in range(n+1)] for j in range(m+1)]
+    # binary representation of each row
+    rowSum = [sum(1 << i for i, c in enumerate(row) if c) for row in g]
     
-    # if last position
-    if g[-1][-1]:
-        # create initial possibilities
-        p1 = copySolution(p)
-        p1[-2][-2], p1[-2][-1], p1[-1][-2], p1[-1][-1] = 0, 0, 0, 1
-        p2 = copySolution(p)
-        p2[-2][-2], p2[-2][-1], p2[-1][-2], p2[-1][-1] = 0, 0, 1, 0
-        p3 = copySolution(p)
-        p3[-2][-2], p3[-2][-1], p3[-1][-2], p3[-1][-1] = 0, 1, 0, 0
-        p4 = copySolution(p)
-        p4[-2][-2], p4[-2][-1], p4[-1][-2], p4[-1][-1] = 1, 0, 0, 0
+    # get all combinations that generate the existing rows
+    uniqueRows = set(rowSum)
+    maxNumber = (1 << cols + 1)
+    possibilities = defaultdict(set)
+    for r1 in range(maxNumber):
+        for r2 in range(maxNumber):
+            # get generated row from each pair of rows
+            result = getResult(r1, r2, cols)
+            # check if generated row belongs to final rows
+            if result in uniqueRows:
+                # any next row r2 transforms current row r1 into result
+                possibilities[(result, r1)].add(r2)
+                
+    # check combined possible combinations for every pair of rows
+    previous = {i : 1 for i in range(maxNumber)}
+    for row in rowSum:
+        nextRow = defaultdict(int)
+        for c in previous:
+            for p in possibilities[(row,c)]:
+                nextRow[p] += previous[c]
+        previous = nextRow
         
-        # execute back propagation to find feasible combinations
-        return backPropagate(g, p1, m-1, n-2) + backPropagate(g, p2, m-1, n-2)\
-            + backPropagate(g, p3, m-1, n-2) + backPropagate(g, p4, m-1, n-2)
-    else:
-        # create initial possibilities
-        p1 = copySolution(p)
-        p1[-2][-2], p1[-2][-1], p1[-1][-2], p1[-1][-1] = 0, 0, 0, 0
-        p2 = copySolution(p)
-        p2[-2][-2], p2[-2][-1], p2[-1][-2], p2[-1][-1] = 0, 0, 1, 1
-        p3 = copySolution(p)
-        p3[-2][-2], p3[-2][-1], p3[-1][-2], p3[-1][-1] = 0, 1, 0, 1
-        p4 = copySolution(p)
-        p4[-2][-2], p4[-2][-1], p4[-1][-2], p4[-1][-1] = 0, 1, 1, 0
-        p5 = copySolution(p)
-        p5[-2][-2], p5[-2][-1], p5[-1][-2], p5[-1][-1] = 0, 1, 1, 1
-        p6 = copySolution(p)
-        p6[-2][-2], p6[-2][-1], p6[-1][-2], p6[-1][-1] = 1, 0, 0, 1
-        p7 = copySolution(p)
-        p7[-2][-2], p7[-2][-1], p7[-1][-2], p7[-1][-1] = 1, 0, 1, 0
-        p8 = copySolution(p)
-        p8[-2][-2], p8[-2][-1], p8[-1][-2], p8[-1][-1] = 1, 0, 1, 1
-        p9 = copySolution(p)
-        p9[-2][-2], p9[-2][-1], p9[-1][-2], p9[-1][-1] = 1, 1, 0, 0
-        p10 = copySolution(p)
-        p10[-2][-2], p10[-2][-1], p10[-1][-2], p10[-1][-1] = 1, 1, 0, 1
-        p11 = copySolution(p)
-        p11[-2][-2], p11[-2][-1], p11[-1][-2], p11[-1][-1] = 1, 1, 1, 0
-        p12 = copySolution(p)
-        p12[-2][-2], p12[-2][-1], p12[-1][-2], p12[-1][-1] = 1, 1, 1, 1
-        
-        # execute back propagation to find feasible combinations
-        return backPropagate(g, p1, m-1, n-2) + backPropagate(g, p2, m-1, n-2) + \
-            backPropagate(g, p3, m-1, n-2) + backPropagate(g, p4, m-1, n-2) + \
-            backPropagate(g, p5, m-1, n-2) + backPropagate(g, p6, m-1, n-2) + \
-            backPropagate(g, p7, m-1, n-2) + backPropagate(g, p8, m-1, n-2) + \
-            backPropagate(g, p9, m-1, n-2) + backPropagate(g, p10, m-1, n-2) + \
-            backPropagate(g, p11, m-1, n-2) + backPropagate(g, p12, m-1, n-2)
-
-solution(g)
+    return sum(previous.values())
